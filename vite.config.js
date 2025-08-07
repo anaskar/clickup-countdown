66665a69394a4da8f89082833596f67b95706b83
anaskar <anaskar@gmail.com>
2025-08-07 18:26:19 -0400

    Create vite.config.js

---
+++ vite.config.js
@@ -0,0 +1,8 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// Base './' ensures assets work under https://<you>.github.io/<repo>/
+export default defineConfig({
+  plugins: [react()],
+  base: './'
+})
